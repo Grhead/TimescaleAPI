966de08522c9812b3bcf2dad45f59489f9411a15
GRHead <egor.mishchuk17@gmail.com>
2026-07-22 17:36:28 +0700

    define endpoints. check file uploading

---
+++ TimescaleAPI/API/TimescaleEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using TimescaleAPI.Application;
+
+namespace TimescaleAPI.API;
+
+public static class TimescaleEndpoints
+{
+    public static void RegisterTimescaleEndpoints(this WebApplication app)
+    {
+        app.MapPost("/upload", async (IFormFile file) =>
+        {
+            using var reader = new StreamReader(file.OpenReadStream());
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" };
+            using var csv = new CsvReader(reader, config);
+            
+            var records = csv.GetRecords<TimescaleData>().ToList();
+            return Results.Ok(records);
+        })
+            .DisableAntiforgery();
+
+        app.MapGet("/results", async () => { });
+
+        app.MapGet("/results/latest", async () => { });
+    }
+}
---
+++ TimescaleAPI/Application/TimescaleData.cs
@@ -0,0 +1,8 @@
+﻿namespace TimescaleAPI.Application;
+
+public class TimescaleData
+{
+    public DateTime Date { get; set; }
+    public int ExecutionTime {get; set;}
+    public double Value {get; set;}
+}
